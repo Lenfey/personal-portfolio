72435ec739478be098965022b0b93dd725d3ddc3
Lenfey <matthiasfeller@hotmail.fr>
2025-04-01 22:57:06 +0200

    Traduction partielle

--- src/components/index.js
+++ src/components/index.js
@@ -4,14 +4,14 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Education from "./Education";
 import SkillsAndExperience from "./SkillsAndExperience";
-//import Projects from "./Projects";
-//import OpenSource from "./OpenSource";
-//import ExtraCurricular from "./ExtraCurricular";
+import Projects from "./Projects";
+import OpenSource from "./OpenSource";
+import ExtraCurricular from "./ExtraCurricular";
 import Footer from "./Footer";
 import Button from "./Button";
 import Stats from "./Stats";
 import Loading from "./Loading";
-//import BlogPosts from "./BlogPosts";
+import BlogPosts from "./BlogPosts";
 import Achievements from "./Achievements";
 
 
@@ -20,13 +20,13 @@ export {
     Hero,
     Education,
     SkillsAndExperience,
-    //Projects,
-    //OpenSource,
-    //ExtraCurricular,
+    Projects,
+    OpenSource,
+    ExtraCurricular,
     Footer,
     Button,
     Stats,
     Loading,
-    //BlogPosts,
+    BlogPosts,
     Achievements,
 };
